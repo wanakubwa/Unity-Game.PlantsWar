3d6641c5350771259f2c4abc7f4fb00f47be7a33
wanakubwa <wanakubwa@gmail.com>
2020-03-19 16:07:03 +0100

    Naprawa rozszerzen klas z kolorami do debugowania.

--- PlantsWar/PlantsWar/Assets/Scripts/Extensions/UnityExtensions.cs
+++ PlantsWar/PlantsWar/Assets/Scripts/Extensions/UnityExtensions.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 public static class UnityExtensions
-{
+{ 
+    public static string ToRGBHex(this Color c)
+    {
+        return string.Format("#{0:X2}{1:X2}{2:X2}", ToByte(c.r), ToByte(c.g), ToByte(c.b));
+    }
+
+    private static byte ToByte(float f)
+    {
+        f = Mathf.Clamp01(f);
+        return (byte)(f * 255);
+    }
+
     public static string SetColor(this string text, Color color)
     {
         string output;
-        output = string.Format("<color = {0}>{1}</color>", color, text);
+        output = string.Format("<color={0}>{1}</color>", color.ToRGBHex(), text);
         return output;
     }
 }
