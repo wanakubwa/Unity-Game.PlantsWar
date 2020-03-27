d1fbe60ed41dc87f39e07432afa8c8604d340a3c
KamilSusek <dumacataloni@gmail.com>
2020-03-27 16:51:47 +0100

    stworznie elementu do setupu postaci gracza w grze

---
+++ PlantsWar/PlantsWar/Assets/Scripts/Generics/SingletonScriptableBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class SingletonScriptableBase<T> : ScriptableObject where T : ScriptableObject
+{
+    static T instance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (instance ==  null)
+            {
+                instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+            }
+
+            return instance;
+        }
+    }
+}
+
---
+++ PlantsWar/PlantsWar/Assets/Scripts/ScriptableObjects/PositiveCharactersContainerSetup.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+[CreateAssetMenu(fileName = "PositiveCharactersContainerSetup.asset", menuName = "Settings/PositiveCharactersContainerSetup")]
+public class PositiveCharactersContainerSetup : SingletonScriptableBase<PositiveCharactersContainerSetup>
+{
+    #region Fields
+
+    [SerializeField]
+    private List<SingleCharacter> characters;
+
+    #endregion
+
+    #region Propeties
+
+    public List<SingleCharacter> Characters { 
+        get => characters; 
+        private set => characters = value; 
+    }
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+
+    #region Handlers
+
+
+
+    #endregion
+
+    [Serializable]
+    public class SingleCharacter
+    {
+        #region Fields
+
+        [SerializeField]
+        private string key;
+
+        [SerializeField]
+        private int prize;
+
+        [SerializeField]
+        private GameObject characterPrefab;
+
+        #endregion
+
+        #region Propeties
+
+        public string Key { 
+            get => key; 
+            private set => key = value; 
+        }
+
+        public int Prize { 
+            get => prize; 
+            private set => prize = value; 
+        }
+
+        public GameObject CharacterPrefab { 
+            get => characterPrefab; 
+            private set => characterPrefab = value; 
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+
+        #region Handlers
+
+
+
+        #endregion
+    }
+}
