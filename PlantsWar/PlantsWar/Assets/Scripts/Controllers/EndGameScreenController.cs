c48bfe98d46194e8a8398e01675b6529cfba57b9
wanakubwa <wanakubwa@gmail.com>
2020-04-07 18:23:09 +0200

    Dodanie kontrolerow i view enkranow konca gry.

---
+++ PlantsWar/PlantsWar/Assets/Scripts/Controllers/EndGameScreenController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class EndGameScreenController : MonoBehaviour
+{
+    #region Fields
+
+
+    #endregion
+
+    #region Propeties
+
+
+
+    #endregion
+
+    #region Methods
+
+    public void ToggleView()
+    {
+        if(gameObject.activeInHierarchy == true)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void ResetGame()
+    {
+        Debug.Log("Reset");
+        ToggleView();
+    }
+
+    public void NextLvl()
+    {
+
+    }
+
+    #endregion
+
+    #region Handlers
+
+
+
+    #endregion
+}
---
+++ PlantsWar/PlantsWar/Assets/Scripts/View/EndGameScreenView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EndGameScreenView : MonoBehaviour
+{
+    #region Fields
+
+    #endregion
+
+    #region Propeties
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+
+    #region Handlers
+
+
+
+    #endregion
+}
