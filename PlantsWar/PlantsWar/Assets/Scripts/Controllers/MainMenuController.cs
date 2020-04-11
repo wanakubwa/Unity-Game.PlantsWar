ef43b4b061528474c8f801148f9c9866d9c574cd
adamskipiotr <adamski-piotrek@wp.pl>
2020-04-11 18:58:03 +0200

    Setup na scenie i szablon kontrollera.

---
+++ PlantsWar/PlantsWar/Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour 
+{
+    #region Fields
+    
+
+    
+    #endregion
+    
+    #region Propeties
+    
+    
+    
+    #endregion
+    
+    #region Methods
+    
+    public void OnNewGameButton()
+    {
+
+    }
+
+    public void OnContinueGameButton()
+    {
+
+    }
+
+    public void OnOptionsButtons()
+    {
+
+    }
+
+    public void OnFacebookButton()
+    {
+        
+    }
+    
+    #endregion
+    
+    #region Handlers
+    
+    
+    
+    #endregion
+    
+    #region Enums
+    
+    
+    
+    #endregion
+} 
