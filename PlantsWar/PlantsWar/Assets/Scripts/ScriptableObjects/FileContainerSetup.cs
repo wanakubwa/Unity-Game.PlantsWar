5f2e7a8de97570489251834cf302773871da386f
wanakubwa <wanakubwa@gmail.com>
2020-04-13 14:07:55 +0200

    Wczytywanie nazw postaci z pliku setup.

--- PlantsWar/PlantsWar/Assets/Scripts/ScriptableObjects/FileContainerSetup.cs
+++ PlantsWar/PlantsWar/Assets/Scripts/ScriptableObjects/FileContainerSetup.cs
@@ -1,34 +1,38 @@
-using UnityEngine;
-using UnityEditor;
 using System;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 [Serializable]
-[CreateAssetMenu(fileName = "FileContainerSetup.asset", menuName = "Settings/FileContainerSetup")]
+[CreateAssetMenu (fileName = "FileContainerSetup.asset", menuName = "Settings/FileContainerSetup")]
 public class FileContainerSetup : ScriptableObject
 {
     #region Fields
-    
+
     private static FileContainerSetup instance;
 
     private const char lineSeparator = '\n';
     private const char fieldSeparator = ',';
+    private const int keyIndex = 0;
 
-    [Space(10)]
+    [Space (10)]
     [SerializeField]
-    private List<FileElement> filesCollection;
-    
+    private List<FileElement> filesCollection = new List<FileElement>();
+
+    [SerializeField, HideInInspector]
+    private Language languageVersion = Language.EN;
+
     #endregion
-    
+
     #region Propeties
-    
+
     public static FileContainerSetup Instance
     {
         get
         {
             if (instance == null)
             {
-                instance = Resources.Load<FileContainerSetup>("Setups/FileContainerSetup");
+                instance = Resources.Load<FileContainerSetup> ("Setups/FileContainerSetup");
             }
 
             return instance;
@@ -39,79 +43,149 @@ public class FileContainerSetup : ScriptableObject
         }
     }
 
-    public List<FileElement> FilesCollection { 
-        get => filesCollection; 
-        private set => filesCollection = value; 
+    public List<FileElement> FilesCollection
+    {
+        get => filesCollection;
+        private set => filesCollection = value;
     }
+
+    public Language LanguageVersion
+    {
+        get => languageVersion;
+        private set => languageVersion = value;
+    }
+
+    public List<string[]> NamesData{
+        get;
+        private set;
+    } = new List<string[]>();
 
     #endregion
 
     #region Methods
 
+    public void SetLanguageVersion (Language language)
+    {
+        LanguageVersion = language;
+    }
 
+    public string GetNameByLocalizedKey(string localizedKey)
+    {
+        for(int i = 0; i < NamesData.Count; i++)
+        {
+            if(NamesData[i][0] == localizedKey)
+            {
+                return NamesData[i][(int)languageVersion];
+            }
+        }
+
+        Debug.LogFormat("Brak nazwy dla klucza {0}!".SetColor(Color.red), localizedKey);
+        return string.Empty;
+    }
+
+    public TextAsset GetTextFileByCategory(FileCategory category)
+    {
+        for(int i = 0; i < FilesCollection.Count; i++)
+        {
+            if(FilesCollection[i].Category == category)
+            {
+                return FilesCollection[i].CsvFile;
+            }
+        }
+
+        Debug.LogFormat("Brak pliku danej kategorii! {0}".SetColor(Color.red), category);
+        return null;
+    }
+
+    private void OnEnable() 
+    {
+        ReadNamesData();
+    }
+
+    private void ReadNamesData()
+    {
+        NamesData.Clear();
+
+        TextAsset file = GetTextFileByCategory(FileCategory.NAMES);
+        if(file == null)
+        {
+            return;
+        }
+
+        string[] lines = file.text.Split(lineSeparator);
+        if(lines != null)
+        {
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(fieldSeparator);
+                NamesData.Add(fields);
+            }
+        }
+
+        Debug.Log("Nazwy wczytwane poprawie".SetColor(Color.green));
+    }
 
     #endregion
 
     #region Handlers
-
-
 
     #endregion
 
     #region Enums
 
     [Serializable]
-    public class FileElement 
+    public class FileElement
     {
         #region Fields
-        
+
         [SerializeField]
-        private FileTypes fileType;
+        private FileCategory category;
 
         [SerializeField]
         private TextAsset csvFile;
-
 
         #endregion
 
         #region Propeties
 
-        public TextAsset CsvFile { 
-            get => csvFile; 
-            private set => csvFile = value; 
+        public TextAsset CsvFile
+        {
+            get => csvFile;
+            private set => csvFile = value;
         }
 
-        public FileTypes FileType { 
-            get => fileType; 
-            private set => fileType = value; 
+        public FileCategory Category
+        {
+            get => category;
+            private set => category = value;
         }
 
         #endregion
 
         #region Methods
 
-
-
         #endregion
 
         #region Handlers
-
-
 
         #endregion
 
         #region Enums
 
-
-
         #endregion
     }
 
-    public enum FileTypes
+    public enum FileCategory
     {
-        CHARACTERS_NAME,
-        CHARACTERS_DESCRIPTION
+        NAMES,
+        DESCRIPTIONS
     }
-    
+
+    public enum Language
+    {
+        PL = 1,
+        EN = 2
+    }
+
     #endregion
 }
