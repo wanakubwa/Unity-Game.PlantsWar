bcd267b07dc3599f8c9241410d42eac8642c4c88
wanakubwa <wanakubwa@gmail.com>
2020-04-16 17:53:34 +0200

    Dodanie szkicu popupu z liczba ukonczonych fal.

---
+++ PlantsWar/PlantsWar/Assets/Scripts/PopUpSystem/WaveEndPopUp/WaveEndPopUpController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WaveEndPopUpController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private WaveEndPopUpView view;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    #endregion
+
+    #region Propeties
+
+    public WaveEndPopUpView View { 
+        get => view; 
+        private set => view = value; 
+    }
+
+    public Canvas Canvas { 
+        get => canvas; 
+        private set => canvas = value; 
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void ClosePopUp()
+    {
+        Destroy(gameObject);
+    }
+
+    public void Initialize()
+    {
+        int wavesNumber = WavesManager.Instance.WavesCounter;
+        View.SetWavesClearedNumber(wavesNumber);
+    }
+
+    public void SetCanvasCamera(Camera camera)
+    {
+        Canvas.worldCamera = camera;
+    }
+
+    #endregion
+
+    #region Handlers
+
+
+
+    #endregion
+}
---
+++ PlantsWar/PlantsWar/Assets/Scripts/PopUpSystem/WaveEndPopUp/WaveEndPopUpView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+
+
+public class WaveEndPopUpView : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private TextMeshProUGUI wavesNumberTMP;
+
+    #endregion
+
+    #region Propeties
+
+    public TextMeshProUGUI WavesNumberTMP { 
+        get => wavesNumberTMP; 
+        private set => wavesNumberTMP = value; 
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void SetWavesClearedNumber(int wavesCleared)
+    {
+        WavesNumberTMP.text = wavesCleared.ToString();
+    }
+
+    #endregion
+
+    #region Handlers
+
+
+
+    #endregion
+}
